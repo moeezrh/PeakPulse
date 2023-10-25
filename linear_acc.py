15cf4904b2499e6e43123ceaecebf18cc98a59b0
xmeatxballx <148910892+xmeatxballx@users.noreply.github.com>
2023-10-24 19:23:35 -0700

    Updated_to_total_linear_acc.py

--- linear_acc.py
+++ linear_acc.py
@@ -4,9 +4,10 @@ Ax = 0.5
 Ay = 0.25
 Az = 0.25
 
-def calc_linear_acc(Ax, Ay):
-    total = math.sqrt(Ax**2 + Ay**2)
-    print("Net acceleration in the x-y plane is ", total)
+def calc_linear_acc(Ax, Ay, Az):
+    xytotal = math.sqrt(Ax**2 + Ay**2)
+    total = math.sqrt(xytotal**2 + Az**2)
+    print("Net acceleration is ", total)
     return total
 
-calc_linear_acc(Ax, Ay)
+calc_total_acc(Ax, Ay, Az)
