8d5e6894a855171d07fc683850488463863cf844
xmeatxballx <michaelticop123@gmail.com>
2023-10-25 18:11:51 -0700

    first draft of peak acc

---
+++ peak_acc.py
@@ -0,0 +1,17 @@
+def peak_acc(given_acc):
+    
+    peak_value = float('-inf')  # Initialize peak value to negative infinity
+
+    while True:
+        try:
+            value = float(given_acc)
+            if value > peak_value:
+                peak_value = value
+            
+
+      
+
+    return(peak_value)
+    
+
+
