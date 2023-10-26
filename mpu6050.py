b3fd636fe8570a7abe6ccdb28c02151fc137700d
Moeez Hussain <moeez123104@gmail.com>
2023-10-25 18:09:19 -0700

    wip

--- mpu6050.py
+++ mpu6050.py
@@ -58,28 +58,30 @@ def read_raw_data(addr):
 # This function is called periodically from FuncAnimation
 def animate(i, s_time, xs, ys):
 
-    # Read data from MPU6050
-    linear_acc_value = calc_linear_acc(read_raw_data(ACCEL_XOUT_H)/16384.0, read_raw_data(ACCEL_ZOUT_H)/16384.0)
+        # Read data from MPU6050
+        linear_acc_value = calc_linear_acc(read_raw_data(ACCEL_XOUT_H)/16384.0, read_raw_data(ACCEL_ZOUT_H)/16384.0)
 
-    # Add x and y to lists
-    
-    xs.append(str(time.time() - s_time))
-    ys.append(linear_acc_value)
+        # Add x and y to lists
+        
+        xs.append(str(time.time() - s_time))
+        ys.append(linear_acc_value)
 
-    # Limit x and y lists to 20 items
-    # xs = xs[-20:]
-    # ys = ys[-20:]
+        # Limit x and y lists to 20 items
+        # xs = xs[-20:]
+        # ys = ys[-20:]
 
-    # Draw x and y lists
-    ax.clear()
-    ax.plot(xs, ys)
-    ax.set_ylim(0, 5)
+        # Draw x and y lists
+        ax.clear()
+        ax.plot(xs, ys)
+        ax.set_ylim(0, 5)
 
-    # Format plot
-    plt.xticks(rotation=45, ha='right')
-    plt.subplots_adjust(bottom=0.30)
-    plt.title('Linear Acceleration over Time')
-    plt.ylabel('Acceleration (g)')
+        # Format plot
+        plt.xticks(rotation=45, ha='right')
+        step = 5
+        ax.set_xticks(x[::step])
+        plt.subplots_adjust(bottom=0.30)
+        plt.title('Linear Acceleration over Time')
+        plt.ylabel('Acceleration (g)')
 
 # Create figure for plotting
 fig = plt.figure()
