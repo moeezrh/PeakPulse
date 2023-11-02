23f5ab53e7e47312fa05454be78fb3923b3316eb
xmeatxballx <michaelticop123@gmail.com>
2023-11-01 18:09:09 -0700

    added angle calc

--- linear_acc.py
+++
@@ -1,7 +0,0 @@
-import math
-
-def calc_linear_acc(axis1, axis2):
-    total_acc_scalar = math.sqrt(axis1**2 + axis2**2)
-    #print("Net acceleration in the x-y plane is ", total)
-    return total
-
