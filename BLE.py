dd5ba8030a4ee9aa08268c5a9959669056a649b5
Moeez Hussain <moeez123104@gmail.com>
2023-11-02 19:50:16 -0700

    ble test

--- BLE.py
+++ BLE.py
@@ -1,4 +1,5 @@
 import bluetooth
+import smbus
 
 # MAC address of the computer you are connected to
 target_address = 'AC:12:03:78:DF:E2'  # Replace this with the computer's MAC address
@@ -9,6 +10,74 @@ port = 4  # The default port for Bluetooth RFCOMM communication
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((target_address, port))
 
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+global end_loop
+
+def MPU_Init():
+	#write to sample rate register
+	bus.write_byte_data(Device_Address, SMPLRT_DIV, 7)
+	
+	#Write to power management register
+	bus.write_byte_data(Device_Address, PWR_MGMT_1, 1)
+	
+	#Write to Configuration register
+	bus.write_byte_data(Device_Address, CONFIG, 0)
+	
+	#Write to Gyro configuration register
+	bus.write_byte_data(Device_Address, GYRO_CONFIG, 24)
+	
+	#Write to interrupt enable register
+	bus.write_byte_data(Device_Address, INT_ENABLE, 1)
+
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+Device_Address = 0x68   # MPU6050 device address
+
+
+MPU_Init()
+
+
+#Read Accelerometer raw value
+acc_x = read_raw_data(ACCEL_XOUT_H)
+acc_y = read_raw_data(ACCEL_YOUT_H)
+acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+
+#Full scale range +/- 250 degree/C as per sensitivity scale factor
+Ax = acc_x/16384.0
+Ay = acc_y/16384.0
+Az = acc_z/16384.0
+
+
+while True:
+    #Read Accelerometer raw value
+    acc_x = read_raw_data(ACCEL_XOUT_H)
+    acc_y = read_raw_data(ACCEL_YOUT_H)
+    acc_z = read_raw_data(ACCEL_ZOUT_H)
+
+
+    #Full scale range +/- 250 degree/C as per sensitivity scale factor
+    Ax = acc_x/16384.0
+    Ay = acc_y/16384.0
+    Az = acc_z/16384.0
+
+    message = "\tAx g: " + str(round(Ax, 2)) + "\tAy g: " + str(round(Ay, 2)) + "\tAz g: " + str(round(Az, 2))
+
+	
 # Data to be sent
 message = "Hello from Raspberry Pi!"
 
