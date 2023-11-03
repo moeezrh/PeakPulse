9eb56f6ba4df59641af13ddabe6089d7de59a03e
Moeez Hussain <moeez123104@gmail.com>
2023-11-02 19:26:34 -0700

    bletest

---
+++ BLE.py
@@ -0,0 +1,19 @@
+import bluetooth
+
+# MAC address of the computer you are connected to
+target_address = 'AC:12:03:78:DF:E2'  # Replace this with the computer's MAC address
+
+port = 1  # The default port for Bluetooth RFCOMM communication
+
+# Establish a connection to the computer
+sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+sock.connect((target_address, port))
+
+# Data to be sent
+message = "Hello from Raspberry Pi!"
+
+# Send the data
+sock.send(message)
+
+# Close the socket
+sock.close()
