5b327a24e98cb49baff8b4de3174e007b96aace7
Moeez Hussain <moeez123104@gmail.com>
2023-11-02 19:59:10 -0700

    ble connection works and transmits acc data

--- User Side Files/BLE_accept.py
+++ User Side Files/BLE_accept.py
@@ -1,5 +1,6 @@
 
 import bluetooth
+import time
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 server_sock.bind(("", bluetooth.PORT_ANY))
@@ -20,12 +21,14 @@ print("Waiting for connection on RFCOMM channel", port)
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from", client_info)
 
+time.sleep(5)
+
 try:
     while True:
         data = client_sock.recv(1024)
         if not data:
             break
-        print("Received", data)
+        print(data)
 except OSError:
     pass
 
