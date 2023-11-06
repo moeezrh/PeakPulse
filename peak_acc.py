ff17577aa48b9315de83499cb469d33e0de141b1
xmeatxballx <michaelticop123@gmail.com>
2023-11-05 21:47:24 -0800

    made edits to the peak acc code

--- peak_acc.py
+++ peak_acc.py
@@ -3,14 +3,12 @@ def peak_acc(given_acc):
     peak_value = float('-inf')  # Initialize peak value to negative infinity
 
     while True:
-        try:
             value = float(given_acc)
+            if value == 1000:
+                 break
             if value > peak_value:
                 peak_value = value
             
-
-      
-
     return(peak_value)
     
 
