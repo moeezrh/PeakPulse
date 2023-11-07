8033d759ac8b4a11aad9992e4b146c9251af3b24
xmeatxballx <michaelticop123@gmail.com>
2023-11-07 11:05:47 -0800

    gui updates

--- GUI.py
+++ GUI.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import font as tkfont
 import subprocess
 
 def run_mpu6050():
@@ -10,18 +11,19 @@ root.title("Welcome to PeakPulse: Choose your exercise")
 root.geometry("400x300")  # Set the window size to 400x300 pixels
 
 # Define a custom font
-customFont = tkfont.Font(family="Helvetica", size=12, weight="bold")
+TitleFont = tkfont.Font(family="Helvetica", size=20, weight="bold")
+ButtonFont = tkfont.Font(family="Helvetica", size=12, weight="bold")
 
 # Create a label widget
-lbl_instruction = tk.Label(root, text="Select the exercise to start:")
+lbl_instruction = tk.Label(root, text="Select Your Exercise:", font=TitleFont, bg="#C2BCBA", fg="white")
 lbl_instruction.pack(pady=10)  # Place the label on the window
 
 # Create buttons to run scripts
-btn_script1 = tk.Button(root, text="Start Running", command=run_mpu6050)
-btn_script2 = tk.Button(root, text="Start Cycling", command=run_mpu6050)  # Placeholder for cycling function
+btn_script1 = tk.Button(root, text="Running", command=run_mpu6050, font=ButtonFont, bg="#9B9391", fg="white")
+btn_script2 = tk.Button(root, text="Placeholder", command=run_mpu6050, font=ButtonFont, bg="#9B9391", fg="white")  # Placeholder 
 
 # Place the buttons on the window using pack
-btn_script1.pack(pady=10)
+btn_script1.pack(pady=40)
 btn_script2.pack(pady=10)
 
 # Start the GUI event loop
