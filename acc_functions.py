dde4a918f527c1877579921344f717db6d0b37ea
xmeatxballx <michaelticop123@gmail.com>
2023-11-06 16:58:16 -0800

    trying out threshold for acc

--- acc_functions.py
+++ acc_functions.py
@@ -3,6 +3,9 @@ import math
 def calc_linear_acc(axis1, axis2):
     total_acc_scalar = math.sqrt(axis1**2 + axis2**2)
     #print("Net acceleration in the x-y plane is ", total)
+    if total_acc_scalar < 0.1:
+        total_acc_scalar == 0
+
     return total_acc_scalar
 
 def calc_acc_angle(axis1,axis2):
