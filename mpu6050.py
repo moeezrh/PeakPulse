6b8aa74db1dc839b3289a7229a0b68ad47eb796e
Moeez Hussain <moeez123104@gmail.com>
2023-11-06 17:01:49 -0800

    returned acceleration animation

--- mpu6050.py
+++ mpu6050.py
@@ -60,6 +60,32 @@ def read_raw_data(addr):
                 value = value - 65536
         return value
 
+# This function is called periodically from FuncAnimation
+def acc_animate(i, s_time, xs, ys):
+
+        # Read data from MPU6050
+        linear_acc_value = calc_linear_acc(read_raw_data(ACCEL_XOUT_H)/16384.0, read_raw_data(ACCEL_ZOUT_H)/16384.0)
+
+        # Add x and y to lists
+
+        xs.append(str(round((time.time() - s_time), 1)))
+        ys.append(linear_acc_value)
+
+        # Limit x and y lists to 20 items
+        # xs = xs[-20:]
+        # ys = ys[-20:]
+
+        # Draw x and y lists
+        ax.clear()
+        ax.plot(xs, ys)
+        ax.set_ylim(0, 5)
+
+        # Format plot
+        plt.xticks(rotation=45, ha='right')
+        plt.subplots_adjust(bottom=0.30)
+        plt.title('Linear Acceleration over Time')
+        plt.ylabel('Acceleration (g)')
+
 
 # def speed_animate(i, s_time, xs, ys):
 
