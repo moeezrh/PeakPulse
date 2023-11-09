f5f171ff048d0b536ad63bd6b68ed213367b9756
xmeatxballx <michaelticop123@gmail.com>
2023-11-08 17:33:43 -0800

    cleaned up mpu6050

--- mpu6050.py
+++ mpu6050.py
@@ -1,7 +1,4 @@
-'''
-        Read Gyro and Accelerometer by Interfacing Raspberry Pi with MPU6050 using Python
-	http://www.electronicwings.com
-'''
+
 import smbus			#import SMBus module of I2C
 import time        
 from time import sleep
@@ -85,33 +82,6 @@ def acc_animate(i, s_time, xs, ys):
         plt.subplots_adjust(bottom=0.30)
         plt.title('Linear Acceleration over Time')
         plt.ylabel('Acceleration (g)')
-
-
-# def speed_animate(i, s_time, xs, ys):
-
-#         # Read data from MPU6050
-#         linear_acc_value = calc_linear_acc(read_raw_data(ACCEL_XOUT_H)/16384.0, read_raw_data(ACCEL_ZOUT_H)/16384.0)
-
-#         # Add x and y to lists
-        
-#         xs.append(str(round((time.time() - s_time), 1)))
-#         ys.append(linear_acc_value)
-
-#         # Limit x and y lists to 20 items
-#         # xs = xs[-20:]
-#         # ys = ys[-20:]
-
-#         # Draw x and y lists
-#         ax.clear()
-#         ax.plot(xs, ys)
-#         ax.set_ylim(0, 5)
-
-#         # Format plot
-#         plt.xticks(rotation=45, ha='right')
-#         plt.subplots_adjust(bottom=0.30)
-#         plt.title('Linear Acceleration over Time')
-#         plt.ylabel('Acceleration (g)')
-
 
 # Create figure for plotting
 fig = plt.figure()
